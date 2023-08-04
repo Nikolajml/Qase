3b4b5d233a2e3d1797bf65e3da99df1ec78272df
Nikolajml <nicolas.maliavko@gmail.com>
2023-08-04 17:47:41 +0300

    Add project files.

---
+++ Qase/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Qase
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Qase/Utilities/Configuration/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qase.Utilities.Configuration
+{
+    public class AppSettings
+    {
+        public string? URL { get; set; }
+    }
+}
